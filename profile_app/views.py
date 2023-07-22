7da4bc782c09bd971e1cfd7f48741e5d059e034d
Gaurav <mgaurav0416@gmail.com>
2023-07-22 10:47:25 +0530

    testing

--- profile_app/views.py
+++ profile_app/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def profile(request):
-    return HttpResponse("this is first deployed project pf django and this is the changes have a great day bro")
+    return render(request,"home.html")
+    #return HttpResponse("this is first deployed project pf django and this is the changes have a great day bro")
