2b248e8b56ef5a5091af0b2606694d8a08d25204
Gaurav <mgaurav0416@gmail.com>
2023-07-23 04:13:36 +0530

    1 commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
